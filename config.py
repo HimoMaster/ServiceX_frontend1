4aa3932595692ccceacab1d635448f8c38e03eb7
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2021-05-18 11:38:05 +0000

    Added a test.

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ Music Player, Telegram Voice Chat Bot
 Copyright (c) 2021  Twist Bots <https://github.com/TwistBots>
 
 This program is free software: you can redistribute it and/or modify
-it under the
+it under the terms of the GNU Affero General Publ
