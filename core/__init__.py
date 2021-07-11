60455028275a85c3fd656f2f4b73a63a8d88fdfc
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2021-07-11 08:58:51 +0000

    Travis update

--- core/__init__.py
+++ core/__init__.py
@@ -3,4 +3,5 @@ Music Player, Telegram Voice Chat Bot
 Copyright (c) 2021  Twist Bots <https://github.com/TwistBots>
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as publish
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, eit
