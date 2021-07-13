bb180b669d50aefce0365d81a3976602d1f4e93e
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2021-07-13 07:45:39 +0000

    Update for.md

--- core/__init__.py
+++ core/__init__.py
@@ -9,4 +9,4 @@ the Free Software Foundation, either version 3 of the License, or
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERC
+MERCHANTABILITY or FITNESS FOR A PARTIC
