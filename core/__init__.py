5ac4001f9fde287381f8c81129d6a1e2bde98e2f
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2021-07-14 16:19:04 +0000

    Create css.css

--- core/__init__.py
+++ core/__init__.py
@@ -10,4 +10,4 @@ the Free Software Foundation, either version 3 of the License, or
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Pu
+GNU Affero General Public License for more detai
