f0e959a3468bfffa20daa15c7f47653789bd0325
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2021-08-01 16:17:21 +0000

    release 1.1.4

--- core/__init__.py
+++ core/__init__.py
@@ -19,3 +19,5 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>
 from core.song import Song
 from core.groups import (
     get_group, get_queue, set_group, set_title, all_groups, clear_queue,
+    set_default, shuffle_queue)
+from co
