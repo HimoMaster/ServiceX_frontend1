6e3d1a2340d06d24378133e6f08b8e83f7ca9333
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2021-08-03 22:47:40 +0000

    Update dbc2.css

--- core/__init__.py
+++ core/__init__.py
@@ -21,4 +21,5 @@ from core.groups import (
     get_group, get_queue, set_group, set_title, all_groups, clear_queue,
     set_default, shuffle_queue)
 from core.funcs import (
-    app, ydl, safone, search, restart, pytgcalls, skip_stre
+    app, ydl, safone, search, restart, pytgcalls, skip_stream, check_yt_url,
+    extract_args, star
