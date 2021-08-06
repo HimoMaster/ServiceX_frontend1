258107b7c805874a2bfb8dc1c8e0c873343d68b9
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2021-08-06 20:41:16 +0000

    Update functionsxenial

--- core/decorators.py
+++ core/decorators.py
@@ -1,3 +1,4 @@
+
 """
 Music Player, Telegram Voice Chat Bot
 Copyright (c) 2021  Twist Bots <https://github.com/TwistBots>
@@ -11,16 +12,3 @@ This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-"""
-
-from core.song import Song
-from core.groups import (
-    get_group, get_queue, set_group, set_title, all_groups, clear_queue,
-    set_default, shuffle_queue)
-from core.funcs import (
-    app, ydl, safone, search, restart, pytgcalls, skip_stream, check_yt_url,
-    extract_args, start_stream, generate_cover, delete_messages,
-    get_youtube_playlist)
