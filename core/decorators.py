e84ff2fe591efd7fe5537cfb56936b16eb2e32c7
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2021-08-08 11:59:58 +0000

    est

--- core/decorators.py
+++ core/decorators.py
@@ -12,3 +12,53 @@ This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
+import time
+from lang import load
+from config import config
+from pyrogram import Client
+from datetime import datetime
+from pytgcalls import PyTgCalls
+from traceback import format_exc
+from pyrogram.types import Message
+from pytgcalls.types import Update
+from typing import Union, Callable
+from core.groups import get_group, all_groups, set_default
+
+
+def register(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message, *args):
+        if message.chat.id not in all_groups():
+            set_default(message.chat.id)
+        return await func(client, message, *args)
+
+    return decorator
+
+
+def language(func: Callable) -> Callable:
+    async def decorator(client, obj: Union[Message, int, Update], *args):
+        try:
+            if isinstance(obj, int):
+                chat_id = obj
+            elif isinstance(obj, Message):
+                chat_id = obj.chat.id
+            elif isinstance(obj, Update):
+                chat_id = obj.chat_id
+            group_lang = get_group(chat_id)["lang"]
+        except BaseException:
+            group_lang = config.LANGUAGE
+        lang = load(group_lang)
+        return await func(client, obj, lang)
+
+    return decorator
+
+
+def only_admins(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message, *args):
+        if (
+            message.from_user.id
+            in [
