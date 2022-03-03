a28a30eacf4001114b8da716298d641bac8a7252
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2022-03-03 05:49:36 +0000

    Disabled greenkeeper-lockfile...

--- core/funcs.py
+++ core/funcs.py
@@ -100,4 +100,5 @@ def search(message: Message) -> Optional[Song]:
 
 def check_yt_url(text: str) -> Tuple[bool, Optional[str]]:
     pattern = re.compile(
-        "^((?:https?:)?\\/\\/)?((?:www|m)\\.)?((?:youtube\\.com|youtu.be))(\\/(?:[\\w\\-]+\\?v=|embed\\/|v\\/)?)([
+        "^((?:https?:)?\\/\\/)?((?:www|m)\\.)?((?:youtube\\.com|youtu.be))(\\/(?:[\\w\\-]+\\?v=|embed\\/|v\\/)?)([\\w\\-]+)([a-zA-Z0-9-_]+)?$"
+    )
