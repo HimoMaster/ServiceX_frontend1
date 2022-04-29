0e292329087ee43f6b402eba9b741d6b5b5bd7ca
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2022-04-29 13:02:40 +0000

    Updated references.

--- core/funcs.py
+++ core/funcs.py
@@ -165,4 +165,5 @@ async def delete_messages(messages):
 async def skip_stream(song: Song, lang):
     chat = song.request_msg.chat
     if safone.get(chat.id) is not None:
-        try
+        try:
+            await safone[chat.id].de
