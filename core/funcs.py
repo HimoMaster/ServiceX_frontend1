fdb75b4c4d052b320ba0b2abbb8c3633c679d44f
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2022-06-22 12:18:38 +0000

    Version bump to 0.9.4

--- core/funcs.py
+++ core/funcs.py
@@ -242,4 +242,5 @@ async def start_stream(song: Song, lang):
 def changeImageSize(maxWidth, maxHeight, image):
     widthRatio = maxWidth / image.size[0]
     heightRatio = maxHeight / image.size[1]
-    newWidth = int(widthRatio * im
+    newWidth = int(widthRatio * image.size[0])
+    newHeight = int(
