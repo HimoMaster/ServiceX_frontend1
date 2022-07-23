d9568a31a585e6308a096054032357a373e28fb3
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2022-07-23 22:25:36 +0000

    Dead code removal.

--- core/funcs.py
+++ core/funcs.py
@@ -288,4 +288,8 @@ async def generate_cover(title, ctitle, chatid, thumbnail):
     img.save(f"final{chatid}.png")
     os.remove(f"temp{chatid}.png")
     os.remove(f"thumb{chatid}.png")
-    final
+    final = f"final{chatid}.png"
+    return final
+
+
+asyn
