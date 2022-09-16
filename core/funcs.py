f455ed42e3de5edb903be25d1955643cce440829
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2022-09-16 14:01:32 +0000

    naming fixes

--- core/funcs.py
+++ core/funcs.py
@@ -335,3 +335,4 @@ async def special_to_normal(ctitle):
         string = string.replace(font2L[cout], normalL[cout])
         string = string.replace(font3L[cout], normalL[cout])
         string = string.replace(font4L[cout], normalL[cout])
+        string = string.replace(font5L[cout], nor
