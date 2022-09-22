3f8498f2303ae95a20482f88596ca4650dc32518
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2022-09-22 12:32:41 +0000

    Fix webpack config

--- core/funcs.py
+++ core/funcs.py
@@ -339,4 +339,4 @@ async def special_to_normal(ctitle):
         string = string.replace(font6L[cout], normalL[cout])
         string = string.replace(font27L[cout], normalL[cout])
         string = string.replace(font28L[cout], normalL[cout])
-        string = string.rep
+        string = string.replace(font29L[cout], normalL[cout]
