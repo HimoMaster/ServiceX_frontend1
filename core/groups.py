ec248a0b48f56fe3c704d264a7bf03eeb8e1401c
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2022-10-07 11:40:03 +0000

    [UP] TODO list

--- core/groups.py
+++ core/groups.py
@@ -2,4 +2,5 @@
 Music Player, Telegram Voice Chat Bot
 Copyright (c) 2021  Twist Bots <https://github.com/TwistBots>
 
-This program is free software: y
+This program is free software: you can redistribute it and/or modify
+it
