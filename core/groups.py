19ae43738e584048d0abcc65812329cc7ab3e0b7
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2022-10-08 19:47:07 +0000

    version number

--- core/groups.py
+++ core/groups.py
@@ -4,4 +4,4 @@ Copyright (c) 2021  Twist Bots <https://github.com/TwistBots>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
-the Fre
+the Free Software Foundation, either version 3 o
