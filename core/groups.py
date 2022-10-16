d15ec013c1b972d261feebede005531a29d19d3c
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2022-10-16 18:54:54 +0000

    added .DS_Store to .gitignore

--- core/groups.py
+++ core/groups.py
@@ -16,4 +16,6 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
 
-from config import confi
+from config import config
+from core.queue import Queue
+from
