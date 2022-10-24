c4c09665c00c6c1a2c9e2e83532bcd8e71e35811
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2022-10-24 10:08:28 +0000

    added pagination

--- core/groups.py
+++ core/groups.py
@@ -37,4 +37,5 @@ def set_default(chat_id: int) -> None:
     GROUPS[chat_id]["is_playing"] = False
     GROUPS[chat_id]["now_playing"] = None
     GROUPS[chat_id]["is_video"] = False
-    GROUPS
+    GROUPS[chat_id]["loop"] = False
+    GROUPS[chat_i
