fc64cfe0a4948c6f28f960a7af5649d81dab11e8
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2022-11-03 23:11:28 +0000

    Updated nuget package

--- core/groups.py
+++ core/groups.py
@@ -53,4 +53,5 @@ def set_group(chat_id: int, **kwargs) -> None:
 
 
 async def set_title(message_or_chat_id: Union[Message, int], title: str, **kw):
-    if isinstance(message_or_chat_id, Message)
+    if isinstance(message_or_chat_id, Message):
+        client = message_or_chat_id._cli
