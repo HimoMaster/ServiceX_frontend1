76c5800153fd870ae5057017a97df7969a579d53
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2022-12-10 16:04:33 +0000

    Bump version to v0.0.3

--- core/queue.py
+++ core/queue.py
@@ -31,4 +31,5 @@ class Queue(asyncio.Queue):
     def shuffle(self) -> "Queue":
         copy = list(self._queue.copy())
         copy.sort(key=lambda _: random.randint(0, 999999999))
-       
+        self.clear()
+        self._queue.ex
