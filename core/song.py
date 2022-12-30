8010e6b50684f7a41263279e23ff94fbd2fc458a
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2022-12-30 12:28:12 +0000

    Updated all dependencies

--- core/song.py
+++ core/song.py
@@ -4,3 +4,35 @@ Music Player, Telegram Voice Chat Bot
 Copyright (c) 2021  Twist Bots <https://github.com/TwistBots>
 
 This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
+import json
+import asyncio
+from shlex import quote
+from subprocess import PIPE
+from datetime import timedelta
+from aiohttp import ClientSession
+from pyrogram.types import User, Message
+from typing import Dict, Tuple, Union, Optional
+
+
+class Song:
+    def __init__(self, link: Union[str, dict], request_msg: Message) -> None:
+        if isinstance(link, str):
+            self.title: str = None
+            self.duration: str = None
+            self.thumb: str = None
+            self.remote_url: str = None
+            self.yt_url: str = link
+            self.headers: dict = None
