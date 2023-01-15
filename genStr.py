d63f233e0e03a1e3cb0ec74eeefa548587aa050f
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2023-01-15 12:47:06 +0000

    [VP] Edited post-meta 'post_views_count' for page 'Open Door Brewing Company'

--- genStr.py
+++ genStr.py
@@ -9,4 +9,5 @@ the Free Software Foundation, either version 3 of the License, or
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  S
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more det
