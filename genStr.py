67b2805b77b0f1dd5ff37431974511692f27006a
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2023-01-29 06:57:59 +0000

    Bump version to 0.0.15

--- genStr.py
+++ genStr.py
@@ -12,4 +12,5 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public Lice
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If
