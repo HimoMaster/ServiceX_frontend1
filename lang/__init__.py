b62df1f9bd7fa6501b9c52dd360d85cb783b35f1
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2023-02-13 16:25:18 +0000

    Added latest Ansible versions

--- lang/__init__.py
+++ lang/__init__.py
@@ -2,4 +2,5 @@
 Music Player, Telegram Voice Chat Bot
 Copyright (c) 2021  Twist Bots <https://github.com/TwistBots>
 
-This program is free software: you can redistribute it and/
+This program is free software: you can redistribute it and/or modify
+it under the terms of the
