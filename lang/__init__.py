7d9d7f61245429c2f3cbb5fbd0aa901dee6131da
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2023-02-17 08:13:30 +0000

    Build unexpected.js

--- lang/__init__.py
+++ lang/__init__.py
@@ -7,4 +7,4 @@ it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-This program is
+This program is distributed in the hope that it will b
