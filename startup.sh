a69f3418e669d664526aa9d803a8321d551fb482
HimoMaster <93041645+HimoMaster@users.noreply.github.com>
2023-04-04 22:02:48 +0000

    updating logo

---
+++ startup.sh
@@ -0,0 +1,28 @@
+
+echo ">> Starting Installation"
+git clone https://github.com/TwistBots/MusicPlayer
+echo ">> Installing Files"
+cd /MusicPlayer
+pip3 install -U -r requirements.txt
+echo ">> Starting Music Player"
+clear
+echo "
+#     #                        
+##   ## #   # #####  # ##### 
+# # # # #   # #      # #    
+#  #  # #   # #####  # #    
+#     # #   #     #  # #    
+#     # ##### #####  # ###### 
+
+                               
+######                                  
+#     # #        ##   #   # ###### ###### 
+#     # #       #  #   # #  #      #    # 
+######  #      #    #   #   #####  #    # 
+#       #      ######   #   #      #####  
+#       #      #    #   #   #      #   #  
+#       ###### #    #   #   ###### #    # 
+                                          
+[ Music Player Is Successfully Deployed ]
+"
+python3 main.py
